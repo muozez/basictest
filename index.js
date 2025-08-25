a3e4bbe7490c28c739f14111e5d075f4113ab7d4
bakbul-muozez <muhammetylmaz128@gmail.com>
2025-08-25 08:27:47 +0300

    fix: update root response message to 'ROAR'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Express!');
+  res.send('ROAR');
 });
 
 app.listen(port, () => {
